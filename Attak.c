f9d3e81b3951d833a00868f6d80a4d8971cb5d8e
SharmaKumarRohit <134692002+SharmaKumarRohit@users.noreply.github.com>
2023-08-23 16:29:58 +0530

    Add files via upload

---
+++ Attak.c
@@ -0,0 +1,183 @@
+// #include<stdio.h>
+// #include<math.h>
+
+// int main(){
+//     int age;
+//     printf("Enter age : ");
+//     scanf("%d", &age);
+
+//     if(age>=18 && age<=50){
+//         printf("You are adult.\n");
+//         printf("They can drive.\n");
+//         printf("They can vote.\n");
+//     }else if(age<18 && age>=13){
+//         printf("You are teenager.\n");
+//         printf("They can not drive.\n");
+//         printf("They can not vote.\n");
+//     }else if(age<13 && age>=5){
+//         printf("You are child.\n");
+//     }else if(age<5 && age>1){
+//         printf("You are small baby.\n");
+//     }else if(age>50 && age<100){
+//         printf("You are oldest man.\n");
+//         printf("They can vote.\n");
+//         printf("They can rest.\n");
+//     }else{
+//         printf("You are enter wrong age.\n");
+//     }
+//     printf("I have a nice day.\n");
+//     printf("Thank you, visit again.");
+
+// Write a program to calculate area of a circle.(radius is given) Radius --> 3.14 * radius * radius.
+// float radius;
+// printf("Enter radius : ");
+// scanf("%f", &radius);
+
+// printf("Area of a circle is : %f", 3.14 * radius * radius);
+
+// Pow(x,y) for x to the power y .
+// int power = pow(2,8);
+// printf("value is : %d", power);
+
+// a. Write a program to cheak if the given number is a natural number.
+// (Natural number start from 1)
+// int number;
+// printf("Enter any number : ");
+// scanf("%d", &number);
+// if(number>0){
+//     printf("This number is natural number.\n");
+// }else{
+//     printf("No, natural number.\n");
+// }
+// int n, sum=0;
+// printf("Enter a number : ");
+// scanf("%d", &n);
+// int i=1;
+// while(i<=n){
+//     sum += i;
+//     i++;
+// }
+// printf("Sum is : %d\n", sum);
+//     return 0;
+// }
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    // int a, b, sum;
+    // printf("Enter a number : ");
+    // scanf("%d", &a);
+    // printf("Enter a number : ");
+    // scanf("%d", &b);
+    // sum = a + b;
+    // printf("%d + %d = %d", a, b, sum);
+
+    // int num1, num2, num3, num4, num5, Avg;
+    // printf("Enter a five number : \n");
+    // scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
+    // Avg = (num1 + num2 + num3 + num4 + num5) / 5;
+    // printf("(%d + %d + %d + %d + %d) / 5 = %d", num1, num2, num3, num4, num5, Avg);
+
+    // int principal, rate, year;
+    // float simple_interest;
+    // printf("1. Enter Principal : ");
+    // scanf("%d", &principal);
+    // printf("2. Enter Rate : ");
+    // scanf("%d", &rate);
+    // printf("3. Enter Loan Year : ");
+    // scanf("%d", &year);
+    // simple_interest = (principal * rate * year) / 100;
+    // printf("(%d X %d X %d) / 100 = %.2f", principal, rate, year, simple_interest);
+
+    // int height, base;
+    // float triangle;
+    // printf("1. Enter height : ");
+    // scanf("%d", &height);
+    // printf("2. Enter base : ");
+    // scanf("%d", &base);
+    // triangle = (height * base) / 2;
+    // printf("(%d X %d) / 2 = %.2f", height, base, triangle);
+
+    // int a, b, c;
+    // float s, area;
+    // printf("1. Enter first number : ");
+    // scanf("%d", &a);
+    // printf("2. Enter second number : ");
+    // scanf("%d", &b);
+    // printf("3. Enter Third number : ");
+    // scanf("%d", &c);
+    // s = (a + b + c) / 2;
+    // area = sqrt(s*(s-a)*(s-b)*(s-c));
+    // printf("(%d + %d + %d) = %.2f\nRoot Under %.2f(%.2f - %d)(%.2f - %d)(%.2f - %d) = %.2f", a, b, c, s, s, s, a, s, b, s, c, area);
+
+    // int a, b, c;
+    // float root1, root2, root_part;
+    // printf("1. Enter First Number : ");
+    // scanf("%d", &a);
+    // printf("2. Enter Second Number : ");
+    // scanf("%d", &b);
+    // printf("3. Enter Third Number : ");
+    // scanf("%d", &c);
+    // root_part = (b*b - 4*a*c);
+    // if(root_part > 0)
+    // {
+    //     root1 = (-b + sqrt(root_part) / 2.0 * a);
+    //     root2 = (-b - sqrt(root_part) / 2.0 * a);
+    //     printf("Root1 = %.2f\nRoot2 = %.2f", root1, root2);
+    // }
+    // else
+    // {
+    //     printf("Error!! Enter another number");
+    // }
+
+    // int a, b, c;
+    // float root1, root2, root_part;
+    // printf("1. Enter First Number : ");
+    // scanf("%d", &a);
+    // printf("2. Enter Second Number : ");
+    // scanf("%d", &b);
+    // printf("3. Enter Third Number : ");
+    // scanf("%d", &c);
+    // root_part = (b*b - 4*a*c);
+    // if(root_part > 0)
+    // {
+    //     root1 = (-b + sqrt(root_part)) / (2.0*a);
+    //     root2 = (-b - sqrt(root_part)) / (2.0*a);
+    //     printf("Root1 = %.2f\nRoot2 = %.2f", root1, root2);
+    // }
+    // else
+    // {
+    //     printf("Error!! Enter another number");
+    // }
+
+    // int a, b, t;
+    // printf("1. Enter First Number : ");
+    // scanf("%d", &a);
+    // printf("2. Enter Second Number : ");
+    // scanf("%d", &b);
+    // printf("Before Number : a = %d & b = %d\n", a, b);
+    // t = a;
+    // a = b;
+    // b = t;
+    // printf("After Number : a = %d & b = %d", a, b);
+
+    // int a, b;
+    // printf("1. Enter First Number : ");
+    // scanf("%d", &a);
+    // printf("2. Enter Second Number : ");
+    // scanf("%d", &b);
+    // printf("Before Number : a = %d & b = %d\n", a, b);
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+    // printf("After Number : a = %d & b = %d", a, b);
+
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    return 0;
+}
